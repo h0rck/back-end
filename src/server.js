e4e608b11bf944d6aa005de85558e21f8959201b
Samuel Ribeiro <samuel.ribeiro@trackcash.com.br>
2021-07-01 22:12:26 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import apis from './apis.js';
+import cors from 'cors'
 // import db from './DB.js';
 const app = express();
 
+
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
